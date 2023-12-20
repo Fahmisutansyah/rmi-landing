7eea97570a4bba53a1eb0eb3e1a6495adf9e35ba
Fahmi Sutansyah <fahmisutansyah@gmail.com>
2023-12-20 11:58:29 +0700

    feat: edit office addresses

--- src/components/footer/FooterArea.vue
+++ src/components/footer/FooterArea.vue
@@ -3,18 +3,18 @@
     <div class="container">
       <div class="footer-up">
         <div class="row">
-          <div class="col-lg-4 col-md-6 col-sm-12">
+          <div class="col-lg-3 col-md-6 col-sm-12">
             <a href="/" class="logo">Raw Material Indonesia</a>
             <p>
               PT. Raw Material Indonesia
             </p>
           </div>
-          <div class="col-lg-2 col-md-6">
+          <div class="col-lg-6 col-md-6">
             <h5>Office</h5>
             <p>
               Jakarta- <br />
-              Gedung Is Plaza, Lantai 8, Ruang 801 <br/>
-              Jl. Pramuka Raya <br/> No. 150, Jakarta Timur.
+              Infiniti Office<br/>
+              Jl. Letjen M.T. Haryono No.Kav 10 GF A4/A, <br/> Cawang, Kecamatan Jatinegara <br/> 13330
             </p>
             <div class="company-email">
               <a href="#">rawmaterial.id@gmail.com</a>
@@ -23,7 +23,7 @@
               <span>+62 812 8595 9576</span>
             </div>
           </div>
-          <div class="col-lg-2 offset-lg-1 col-md-6 com-sm-12">
+          <!-- <div class="col-lg-2 offset-lg-1 col-md-6 com-sm-12"> -->
             <!-- <h5>Links</h5>
             <ul>
               <li>
@@ -31,7 +31,7 @@
                 <router-link :to="{ name: 'services' }">Products</router-link>
               </li>
             </ul> -->
-          </div>
+          <!-- </div> -->
           <div class="col-lg-3 col-md-6 col-sm-12">
             <h5>Get in Touch</h5>
             <ul>
